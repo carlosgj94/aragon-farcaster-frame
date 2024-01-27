bc48d148eb1b802e03b75468ddc9b38b10b90c72
Carlos Juarez <carlosgj94@gmail.com>
2024-01-27 19:25:45 +0100

    style: adding the initial design for the svg

--- pages/api/image.tsx
+++ pages/api/image.tsx
@@ -4,7 +4,8 @@ import satori from "satori";
 import { join } from 'path';
 import * as fs from "fs";
 
-const fontPath = join(process.cwd(), 'Roboto-Regular.ttf')
+const fontPath = join(process.cwd(), 'Manrope-Regular.ttf')
+// const fontPath = join(process.cwd(), 'Roboto-Regular.ttf')
 let fontData = fs.readFileSync(fontPath)
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
@@ -22,52 +23,99 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
         display: 'flex',
         width: '100%',
         height: '100%',
-        backgroundColor: 'f4f4f4',
-        padding: 50,
-        lineHeight: 1.2,
-        fontSize: 24,
+        backgroundColor: 'white',
+        padding: 24,
+        fontSize: 14,
       }}>
         <div style={{
           display: 'flex',
           flexDirection: 'column',
-          padding: 20,
+          width: '100%',
         }}>
-          <h2 style={{ textAlign: 'center', color: 'lightgray' }}>This is an Aragon proposal title</h2>
           <div style={{
-            backgroundColor: '#007bff',
-            color: '#fff',
-            padding: 10,
-            marginBottom: 10,
-            borderRadius: 4,
-            width: `20%`,
-            whiteSpace: 'nowrap',
-            overflow: 'visible',
-          }}>Yes</div>
+            display: 'flex',
+            flexDirection: 'row',
+            justifyContent: 'space-between',
+          }}>
+            <div style={{ display: 'flex' }}>
+              <img src="https://pbs.twimg.com/profile_images/1696708161103921152/_6R6pJoS_400x400.jpg"
+                alt="Trulli"
+                width="50"
+                height="50"
+                style={{ borderRadius: '50%' }}
+              />
+              <h2 style={{ textAlign: 'center', color: '#323F4B', fontSize: '1.3rem', paddingLeft: 6 }}>
+                Devolved AI Treasury DAO
+              </h2>
+            </div>
+
+            <h2 style={{ textAlign: 'center', color: '#616E7C', fontSize: '1.1rem' }}>
+              Powered by <b style={{ color: '#3164FA' }}>&nbsp;Aragon</b>
+            </h2>
+
+          </div>
+          <h1 style={{ textAlign: 'start', color: '#323F4B', fontSize: 30, lineHeight: 1, marginBottom: 0, marginTop: 0.6 }}>
+            This is an Aragon proposal title
+          </h1>
+
+          <p style={{ textAlign: 'start', color: '#616E7C', fontSize: 22 }}>
+            This is an Aragon proposal description. This is an Aragon proposal description. This is an Aragon proposal description.
+          </p>
+
+
           <div style={{
-            backgroundColor: '#007bff',
-            color: '#fff',
-            padding: 10,
-            marginBottom: 10,
-            borderRadius: 4,
-            width: `60%`,
-            whiteSpace: 'nowrap',
-            overflow: 'visible',
-          }}>No</div>
-          <div style={{
-            backgroundColor: '#007bff',
-            color: '#fff',
-            padding: 10,
-            marginBottom: 10,
-            borderRadius: 4,
-            width: `20%`,
-            whiteSpace: 'nowrap',
-            overflow: 'visible',
-          }}>Abstain</div>
+            display: 'flex',
+            flexDirection: 'column',
+            border: '1px',
+            borderColor: '#E4E7EB',
+            borderRadius: '8px',
+            marginTop: 5,
+            padding: 15,
+            boxShadow: '0px 1px 2px 0px rgba(97, 110, 124, 0.05)',
+          }}>
+            <div style={{ display: 'flex', flexDirection: 'row', justifyContent: 'space-between' }}>
+              <p style={{ color: '#323F4B', fontSize: '1.25rem', margin: '0.5em' }}>
+                Winning Option
+              </p>
+              <p style={{ color: '#616E7C', fontSize: '1.25rem', margin: '0.5em' }}>
+                64%
+              </p>
+            </div>
+
+            <div style={{
+              display: 'flex',
+              backgroundColor: '#E4E7EB',
+              borderRadius: 12,
+              width: `100%`,
+              whiteSpace: 'nowrap',
+              overflow: 'visible',
+            }}>
+              <div style={{
+                backgroundColor: '#007bff',
+                padding: 10,
+                borderRadius: 12,
+                width: `64%`,
+                whiteSpace: 'nowrap',
+                overflow: 'visible',
+              }}></div>
+            </div>
+
+            <div style={{ display: 'flex', flexDirection: 'row', justifyContent: 'space-between' }}>
+              <p style={{ color: '#3164FA', fontSize: '1.3rem', margin: '0.5em' }}>
+                No
+              </p>
+              <p style={{ color: '#616E7C', fontSize: '1.3rem', margin: '0.5em' }}>
+                3.5M wANT
+              </p>
+            </div>
+
+
+          </div>
         </div>
       </div>
       ,
       {
-        width: 600, height: 400, fonts: [{ data: fontData, name: 'Roboto', weight: 400, style: 'normal', },],
+        width: 600, height: 400, fonts: [{ data: fontData, name: 'Manrope', weight: 400, style: 'normal', },],
       })
     // Convert SVG to PNG using Sharp
     const pngBuffer = await sharp(Buffer.from(svg))
